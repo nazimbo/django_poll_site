ec293c0591a22a442cd772815110ab37104b632a
nazimbo <113207575+nazimbo@users.noreply.github.com>
2024-06-15 21:04:48 +0200

    Add __str__ method to models

--- polls/models.py
+++ polls/models.py
@@ -2,19 +2,25 @@ from django.db import models
 
 # The following code defines the database schema for the polls application.
 # It creates the following tables:
+
+
 # - Question
-
-
 class Question(models.Model):
     # Each field is represented by an instance of a Field class.
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+    # The __str__() method is the default human-readable representation of the object.
+    def __str__(self):
+        return self.question_text
+
+
 # - Choice
-
-
 class Choice(models.Model):
     # The ForeignKey tells Django that each Choice is related to a single Question.
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
